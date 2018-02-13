44a8ef934fc72f4f1c6e131da17b70bb5de4e7ca
Moritz Mueller <moritz@schaefermueller.de>
2018-02-13 16:32:45 +0100

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
+1.times do
  task =  Task.new(title: "Clean Garage", details:"Higly important")
  task.save
 end
 
-5.times do
+1.times do
  task =  Task.new(title: "Garden work", details:"only front")
  task.save
 end
 
-5.times do
+1.times do
  task =  Task.new(title: "Do stuff", details:" just do it")
  task.save
 end
 
-5.times do
+1.times do
  task =  Task.new(title: "something", details:"not so important")
+ task.save
+end
+
+1.times do
+ task =  Task.new(title: "Bring kids to school", details:"before 8am")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "do this", details:"not so important")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "do that", details:"not so important")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "make that", details:"not so important")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "make something", details:"not so important")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "Buy gifts", details:"not so important")
+ task.save
+end
+1.times do
+ task =  Task.new(title: "buy new car", details:"not so important")
  task.save
 end
